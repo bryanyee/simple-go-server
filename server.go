594384b62c1c808934a2f1b010de3f935b658eee
Bryan Yee <yee.bryan.c@gmail.com>
2021-06-20 15:26:45 -0700

    Setup project

---
+++ server.go
@@ -0,0 +1,13 @@
+package main
+
+// https://blog.logrocket.com/creating-a-web-server-with-golang/
+
+import (
+	"fmt"
+	// "log"
+	// "net/http"
+)
+
+func main() {
+	fmt.Printf("Starting server at port 8080\n")
+}
